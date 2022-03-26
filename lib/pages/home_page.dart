e10a67950e7ae4be832dd881b0e85e55796b791a
Suraj Upadhyay <usuraj35@gmail.com>
2022-03-26 11:29:44 +0530

    Add Item Widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,30 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
+import 'package:learning_project/models/catalog.dart';
 import 'package:learning_project/widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const int days = 30;
-    const String name = "Suraj";
+    final dummyList = List.generate(20, ((index) => CatalogModel.items));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Learning App"),
       ),
-      body: const Material(
-        child: Center(
-          child: Text("Welcome $name to $days days of flutter"),
+      body: Material(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ItemWidget(item: dummyList[index][0]);
+            },
+          ),
         ),
       ),
       drawer: MyDrawer(),
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:learning_project/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}"),
+      ),
+    );
+  }
+}
