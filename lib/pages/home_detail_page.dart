f1e796b433605c466d38b76dc4da000f7987f7d1
Suraj Upadhyay <usuraj35@gmail.com>
2022-04-05 21:16:19 +0530

    Add product details

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -1,14 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:learning_project/widgets/item_widgets/buy_action.dart';
+import 'package:learning_project/widgets/my_themes.dart';
 
-import "package:flutter/material.dart";
+import '../models/catalog.dart';
 
 class HomeDetails extends StatelessWidget {
-  const HomeDetails({ Key? key }) : super(key: key);
+  final Item catalog;
+
+  const HomeDetails({Key? key, required this.catalog}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      backgroundColor: MyThemes.creamColor,
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width,
+              color: MyThemes.creamColor,
+              child: Hero(
+                tag: Key(catalog.id.toString()),
+                child: Image.network(catalog.image),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: MyThemes.creamColor,
+                child: ClipPath(
+                  clipper: MyArcClipper(),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.white,
+                    child: ProductDetail(
+                      catalog: catalog,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyArcClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.quadraticBezierTo(size.width / 2, size.height / 6, size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+class ProductDetail extends StatelessWidget {
+  final Item catalog;
+  const ProductDetail({Key? key, required this.catalog}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  catalog.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textScaleFactor: 1.9,
+                ),
+                Text(
+                  catalog.desc,
+                  textScaleFactor: 1.3,
+                ),
+              ],
+            ),
+            BuyAction(price: catalog.price)
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learning_project/models/catalog.dart';
+import 'package:learning_project/pages/home_detail_page.dart';
 import 'package:learning_project/widgets/item_widgets/product_details.dart';
 
 import 'item_widgets/my_image.dart';
@@ -21,23 +22,31 @@ class MyListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 140,
-      alignment: Alignment.center,
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0.0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20), color: Colors.white),
-      padding: const EdgeInsets.fromLTRB(20, 15, 8, 15),
-      child: Row(
-        children: [
-          MyImage(image: item.image),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5.0),
-            child: ProductDetails(
-                name: item.name, desc: item.desc, price: item.price),
-          ))
-        ],
+    return InkWell(
+      onTap: () => Navigator.push(context,
+          MaterialPageRoute(builder: (context) => HomeDetails(catalog: item))),
+      child: Container(
+        height: 140,
+        alignment: Alignment.center,
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: Colors.white),
+        padding: const EdgeInsets.fromLTRB(20, 15, 8, 15),
+        child: Row(
+          children: [
+            Hero(
+              tag: Key(item.id.toString()),
+              child: MyImage(image: item.image),
+            ),
+            Expanded(
+                child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10, vertical: 5.0),
+              child: ProductDetails(
+                  name: item.name, desc: item.desc, price: item.price),
+            ))
+          ],
+        ),
       ),
     );
   }
